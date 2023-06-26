0ae125b0087e8f8f7db7394816f47b2736f9bb5c
selvam920 <skssoftware@live.com>
2023-06-26 08:06:09 +0530

    updated

--- lib/drago_pos_printer.dart
+++ lib/drago_pos_printer.dart
@@ -3,7 +3,7 @@ export 'package:drago_pos_printer/models/bluetooth_printer.dart';
 export 'package:drago_pos_printer/models/usb_printer.dart';
 
 export 'package:drago_pos_printer/services/bluetooth_printer_manager_web.dart'
-    if (dart.library.html) 'package:drago_pos_printer/services/bluetooth_printer_manager_web.dart'
+    if (dart.library.js) 'package:drago_pos_printer/services/bluetooth_printer_manager_web.dart'
     if (dart.library.io) 'package:drago_pos_printer/services/bluetooth_printer_manager.dart';
 
 // export 'package:drago_pos_printer/services/bluetooth_printer_manager.dart'
@@ -14,7 +14,7 @@ export 'package:drago_pos_printer/services/network_printer_manager.dart';
 //     if (dart.library.js) 'package:drago_pos_printer/services/usb_printer_manager_web.dart';
 
 export 'package:drago_pos_printer/services/usb_printer_manager_web.dart'
-    if (dart.library.html) 'package:drago_pos_printer/services/usb_printer_manager_web.dart'
+    if (dart.library.js) 'package:drago_pos_printer/services/usb_printer_manager_web.dart'
     if (dart.library.io) 'package:drago_pos_printer/services/usb_printer_manager.dart';
 
 export 'package:drago_pos_printer/enums/bluetooth_printer_type.dart';
