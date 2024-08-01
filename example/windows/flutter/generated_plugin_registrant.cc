6e9ca5a3c4da7f2d68904c1cc933cdc1cbb4fae1
selvam920 <skssoftware@live.com>
2024-08-01 08:58:07 +0530

    upgrade packages

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,15 +7,15 @@
 #include "generated_plugin_registrant.h"
 
 #include <charset_converter/charset_converter_plugin.h>
-#include <drago_pos_printer/drago_pos_printer_plugin.h>
+#include <drago_pos_printer/drago_pos_printer_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <printing/printing_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   CharsetConverterPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CharsetConverterPlugin"));
-  DragoPosPrinterPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DragoPosPrinterPlugin"));
+  DragoPosPrinterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DragoPosPrinterPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   PrintingPluginRegisterWithRegistrar(
