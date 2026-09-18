122068a5e826a4d75b6888f22158e6a947fd007a
selvam920 <skssoftware@live.com>
2026-09-19 05:08:13 +0530

    Set Android NDK version
    Explicitly configure the Android app's NDK version to 30.0.16248370 to match the required toolchain and avoid build issues with newer Flutter/Gradle setups.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -4,9 +4,11 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
 android {
     namespace = "com.example.drago_pos_printer_example"
     compileSdk = 37
+    ndkVersion = "30.0.16248370"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
